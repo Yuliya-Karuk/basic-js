99f00795f0f677f90b3c3aa7ee0c784862920ede
Yuliya-Karuk <karuk.yuliya@gmail.com>
2023-09-12 18:03:34 +0400

    feat: solve dns-stats and dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,13 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (Array.isArray(members)) {
+    let preparedMembers = members.filter((el) => typeof el === 'string').map(el => el.trim().toUpperCase());
+    let team = preparedMembers.sort().reduce(((acc, el) => acc + el[0]), '')
+    return team;
+  }
+  return false;
 }
 
 module.exports = {
